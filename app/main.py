feb27dff3abe0f3dbffdd7ccb9efd7046fbe213f
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-03-13 02:01:06 +0100

    feature: create User model

--- app/main.py
+++ app/main.py
@@ -6,13 +6,14 @@ from routers.api import router
 from models.offer import Offer
 from models.location import Region, City
 from models.photo import Photo
+from models.user import User
 
 Base.metadata.create_all(bind=engine)
 Offer.metadata.create_all(bind=engine)
 Region.metadata.create_all(bind=engine)
 Photo.metadata.create_all(bind=engine)
 City.metadata.create_all(bind=engine)
-
+User.metadata.create_all(bind=engine)
 
 app = FastAPI(
     debug=bool(settings.DEBUG),
---
+++ app/models/user.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column
+from sqlalchemy import Integer, String, Boolean
+
+from config.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_superuser = Column(Boolean, default=False)
+    is_active = Column(Boolean, default=True)
